9ad02c2b3bce7bdf4e7481727cb34bb20beca9df
Karol <karolinedalencar@gmail.com>
2019-10-19 17:53:43 -0300

    Update build file

--- bin/main.js
+++ bin/main.js
@@ -5,6 +5,6 @@ const program = require("commander");
 const pkg = require("../package.json");
 const searchWord = require("./searchWord");
 
-program.description("A CLI that shows the definition of a word.").option("-W, --word <word>", "Word to be search.").version(pkg.version).parse(process.argv);
+program.description("A CLI that shows the definition of a word.").option("-W, --word <word>", "word to be search").version(pkg.version).parse(process.argv);
 
 searchWord(program.word);
