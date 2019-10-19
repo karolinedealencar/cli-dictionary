4fd7731f0e5a82dcf50ed97fa3c4dc457ad4d5c7
Karol <karolinedalencar@gmail.com>
2019-10-19 17:48:22 -0300

    Change word desc

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ const searchWord = require("./searchWord");
 
 program
   .description("A CLI that shows the definition of a word.")
-  .option("-W, --word <word>", "Word to be search.")
+  .option("-W, --word <word>", "word to be search")
   .version(pkg.version)
   .parse(process.argv);
 
