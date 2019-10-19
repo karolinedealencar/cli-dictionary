68dc1d4dbbe80bf98c152e34eef6b79379a9363a
Karoline Alencar <karolinedalencar@gmail.com>
2019-10-19 20:32:03 -0300

    Add spinner

--- bin/searchWord.js
+++ bin/searchWord.js
@@ -1,6 +1,13 @@
 const fetch = require("node-fetch");
+const ora = require("ora");
+
+const spinner = ora({
+  text: "Loading definition..."
+});
 
 function searchWord(word = "love") {
+  spinner.start();
+
   return fetch(`https://owlbot.info/api/v3/dictionary/${word}`, {
     headers: {
       Authorization: "Token 07f42659d3d7051946c054e82d690c93ced5bcd4"
@@ -8,7 +15,11 @@ function searchWord(word = "love") {
   }).then(response => {
     if (response.ok) return response.json();
     throw new Error();
-  }).then(response => console.log(response.definitions[0].definition)).catch(error => {
+  }).then(response => {
+    spinner.stop();
+    console.log(`${word}: ${response.definitions[0].definition}`);
+  }).catch(error => {
+    spinner.stop();
     console.log("something went wrong, try again.");
     throw error;
   });
